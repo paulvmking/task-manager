3eb99cd3ba139fae2723b65180418e081a491e28
paulvmking <paulvmking@gmail.com>
2021-05-12 18:04:23 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
