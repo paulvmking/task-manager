9856df86c9cba50130f30a58a29b5cd2fcbade14
paulvmking <paulvmking@gmail.com>
2021-05-12 16:45:57 +0000

    Bind data from db to accordian

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
 });
