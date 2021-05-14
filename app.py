c20d7e170840d3e108e792faf9d105dd73a95b84
paulvmking <paulvmking@gmail.com>
2021-05-14 14:47:54 +0000

    Wire up task edit button

--- app.py
+++ app.py
@@ -119,6 +119,13 @@ def add_task():
     return render_template("add_task.html", categories=categories)
 
 
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_task.html", task=task, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
